3b2309f210417b725a35c3c0d6061f0063f21385
AKHOUN Assad <AKHOUNA@sio.lan>
2026-04-02 15:07:52 +0400

    feature : ajout classe CONNEXION BDD - gestion connexion MySQL

---
+++ gsb_ap8/ConnexionBDD.vb
@@ -0,0 +1,19 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class ConnexionBDD
+
+    Private Shared _connexion As MySqlConnection
+
+    Public Shared Function GetConnexion() As MySqlConnection
+        If _connexion Is Nothing Then
+            Dim _connexion As New MySqlConnection(ModuleConfig.GetConnectionString())
+        End If
+
+        If _connexion.State = ConnectionState.Closed Then
+            _connexion.Open()
+        End If
+
+        Return _connexion
+    End Function
+
+End Class
