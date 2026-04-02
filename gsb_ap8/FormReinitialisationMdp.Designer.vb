851998d76293ccc8fadd70833116fb19eafe06e6
AKHOUN Assad <AKHOUNA@sio.lan>
2026-04-02 17:11:42 +0400

    feature : ajout FormReinitialisationMdp - réinitialisation mot de passe utilisateur

--- gsb_ap8/FormReinitialisationMdp.Designer.vb
+++ gsb_ap8/FormReinitialisationMdp.Designer.vb
@@ -22,9 +22,98 @@ Partial Class FormReinitialisationMdp
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.lblUtilisateur = New System.Windows.Forms.Label()
+        Me.lblNouveauMdp = New System.Windows.Forms.Label()
+        Me.lblConfirmerMdp = New System.Windows.Forms.Label()
+        Me.cmbUtilisateurs = New System.Windows.Forms.ComboBox()
+        Me.txtNouveauMdp = New System.Windows.Forms.TextBox()
+        Me.txtConfirmerMdp = New System.Windows.Forms.TextBox()
+        Me.btnValider = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUtilisateur
+        '
+        Me.lblUtilisateur.AutoSize = True
+        Me.lblUtilisateur.Location = New System.Drawing.Point(94, 48)
+        Me.lblUtilisateur.Name = "lblUtilisateur"
+        Me.lblUtilisateur.Size = New System.Drawing.Size(128, 13)
+        Me.lblUtilisateur.TabIndex = 0
+        Me.lblUtilisateur.Text = "Sélectionner un utilisateur"
+        '
+        'lblNouveauMdp
+        '
+        Me.lblNouveauMdp.AutoSize = True
+        Me.lblNouveauMdp.Location = New System.Drawing.Point(94, 135)
+        Me.lblNouveauMdp.Name = "lblNouveauMdp"
+        Me.lblNouveauMdp.Size = New System.Drawing.Size(117, 13)
+        Me.lblNouveauMdp.TabIndex = 1
+        Me.lblNouveauMdp.Text = "Nouveau mot de passe"
+        '
+        'lblConfirmerMdp
+        '
+        Me.lblConfirmerMdp.AutoSize = True
+        Me.lblConfirmerMdp.Location = New System.Drawing.Point(94, 238)
+        Me.lblConfirmerMdp.Name = "lblConfirmerMdp"
+        Me.lblConfirmerMdp.Size = New System.Drawing.Size(128, 13)
+        Me.lblConfirmerMdp.TabIndex = 2
+        Me.lblConfirmerMdp.Text = "Confirmer le mot de passe"
+        '
+        'cmbUtilisateurs
+        '
+        Me.cmbUtilisateurs.FormattingEnabled = True
+        Me.cmbUtilisateurs.Location = New System.Drawing.Point(90, 96)
+        Me.cmbUtilisateurs.Name = "cmbUtilisateurs"
+        Me.cmbUtilisateurs.Size = New System.Drawing.Size(121, 21)
+        Me.cmbUtilisateurs.TabIndex = 3
+        '
+        'txtNouveauMdp
+        '
+        Me.txtNouveauMdp.Location = New System.Drawing.Point(97, 188)
+        Me.txtNouveauMdp.Name = "txtNouveauMdp"
+        Me.txtNouveauMdp.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNouveauMdp.Size = New System.Drawing.Size(100, 20)
+        Me.txtNouveauMdp.TabIndex = 4
+        '
+        'txtConfirmerMdp
+        '
+        Me.txtConfirmerMdp.Location = New System.Drawing.Point(97, 293)
+        Me.txtConfirmerMdp.Name = "txtConfirmerMdp"
+        Me.txtConfirmerMdp.Size = New System.Drawing.Size(100, 20)
+        Me.txtConfirmerMdp.TabIndex = 5
+        '
+        'btnValider
+        '
+        Me.btnValider.Location = New System.Drawing.Point(122, 360)
+        Me.btnValider.Name = "btnValider"
+        Me.btnValider.Size = New System.Drawing.Size(75, 23)
+        Me.btnValider.TabIndex = 6
+        Me.btnValider.Text = "Valider"
+        Me.btnValider.UseVisualStyleBackColor = True
+        '
+        'FormReinitialisationMdp
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1336, 450)
+        Me.Controls.Add(Me.btnValider)
+        Me.Controls.Add(Me.txtConfirmerMdp)
+        Me.Controls.Add(Me.txtNouveauMdp)
+        Me.Controls.Add(Me.cmbUtilisateurs)
+        Me.Controls.Add(Me.lblConfirmerMdp)
+        Me.Controls.Add(Me.lblNouveauMdp)
+        Me.Controls.Add(Me.lblUtilisateur)
+        Me.Name = "FormReinitialisationMdp"
         Me.Text = "FormReinitialisationMdp"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents lblUtilisateur As Label
+    Friend WithEvents lblNouveauMdp As Label
+    Friend WithEvents lblConfirmerMdp As Label
+    Friend WithEvents cmbUtilisateurs As ComboBox
+    Friend WithEvents txtNouveauMdp As TextBox
+    Friend WithEvents txtConfirmerMdp As TextBox
+    Friend WithEvents btnValider As Button
 End Class
--- gsb_ap8/FormReinitialisationMdp.vb
+++ gsb_ap8/FormReinitialisationMdp.vb
@@ -1,3 +1,34 @@
-﻿Public Class FormReinitialisationMdp
+﻿Imports MySql.Data.MySqlClient
 
+Public Class FormReinitialisationMdp
+    Private Sub FormReinitialisationMdp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Charger les utilisateurs dans le ComboBox
+        Dim requete = "SELECT id, CONCAT(nom, ' ', prenom) AS nomComplet FROM utilisateur ORDER BY nom"
+        Dim adapter As New MySqlDataAdapter(requete, ConnexionBDD.GetConnexion())
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        cmbUtilisateurs.DataSource = table
+        cmbUtilisateurs.DisplayMember = "nomComplet"
+        cmbUtilisateurs.ValueMember = "id"
+    End Sub
+
+    Private Sub btnValider_Click(sender As Object, e As EventArgs) Handles btnValider.Click
+        If txtNouveauMdp.Text = txtConfirmerMdp.Text Then
+            Dim nouveauMdpHashe = HashSHA256(txtNouveauMdp.Text)
+
+            Dim requete = "UPDATE utilisateur SET mdp = @mdp WHERE id = @id"
+            Dim cmd As New MySqlCommand(requete, ConnexionBDD.GetConnexion())
+            cmd.Parameters.AddWithValue("@mdp", nouveauMdpHashe)
+            cmd.Parameters.AddWithValue("@id", cmbUtilisateurs.SelectedValue)
+            cmd.ExecuteNonQuery()
+
+            MessageBox.Show("Mot de passe réinitialisé avec succès.", "Succès",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information)
+            txtNouveauMdp.Clear()
+            txtConfirmerMdp.Clear()
+        Else
+            MessageBox.Show("Les mots de passe ne correspondent pas.", "Erreur",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
 End Class
