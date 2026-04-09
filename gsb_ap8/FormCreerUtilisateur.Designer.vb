bf8ea54e4fb547178cf59c034680112aa8d86685
AKHOUN Assad <AKHOUNA@sio.lan>
2026-04-09 15:10:24 +0400

    feat: ajout FormCreerUtilisateur - création visiteur et comptable

--- gsb_ap8/FormCreerUtilisateur.Designer.vb
+++ gsb_ap8/FormCreerUtilisateur.Designer.vb
@@ -22,9 +22,130 @@ Partial Class FormCreerUtilisateur
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.txtNom = New System.Windows.Forms.TextBox()
+        Me.txtPrenom = New System.Windows.Forms.TextBox()
+        Me.txtLogin = New System.Windows.Forms.TextBox()
+        Me.txtMdp = New System.Windows.Forms.TextBox()
+        Me.txtAdresse = New System.Windows.Forms.TextBox()
+        Me.txtCodePostal = New System.Windows.Forms.TextBox()
+        Me.txtVille = New System.Windows.Forms.TextBox()
+        Me.dtpEmbauche = New System.Windows.Forms.DateTimePicker()
+        Me.cmbRole = New System.Windows.Forms.ComboBox()
+        Me.btnValider = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNom
+        '
+        Me.txtNom.Location = New System.Drawing.Point(87, 32)
+        Me.txtNom.Name = "txtNom"
+        Me.txtNom.Size = New System.Drawing.Size(100, 20)
+        Me.txtNom.TabIndex = 0
+        Me.txtNom.Text = "Nom"
+        '
+        'txtPrenom
+        '
+        Me.txtPrenom.Location = New System.Drawing.Point(87, 86)
+        Me.txtPrenom.Name = "txtPrenom"
+        Me.txtPrenom.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrenom.TabIndex = 1
+        Me.txtPrenom.Text = "Prénom"
+        '
+        'txtLogin
+        '
+        Me.txtLogin.Location = New System.Drawing.Point(87, 147)
+        Me.txtLogin.Name = "txtLogin"
+        Me.txtLogin.Size = New System.Drawing.Size(100, 20)
+        Me.txtLogin.TabIndex = 2
+        Me.txtLogin.Text = "Identifiant"
+        '
+        'txtMdp
+        '
+        Me.txtMdp.Location = New System.Drawing.Point(87, 207)
+        Me.txtMdp.Name = "txtMdp"
+        Me.txtMdp.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtMdp.Size = New System.Drawing.Size(100, 20)
+        Me.txtMdp.TabIndex = 3
+        Me.txtMdp.Text = "Mot de passe"
+        '
+        'txtAdresse
+        '
+        Me.txtAdresse.Location = New System.Drawing.Point(87, 257)
+        Me.txtAdresse.Name = "txtAdresse"
+        Me.txtAdresse.Size = New System.Drawing.Size(100, 20)
+        Me.txtAdresse.TabIndex = 4
+        Me.txtAdresse.Text = "Adresse"
+        '
+        'txtCodePostal
+        '
+        Me.txtCodePostal.Location = New System.Drawing.Point(87, 303)
+        Me.txtCodePostal.Name = "txtCodePostal"
+        Me.txtCodePostal.Size = New System.Drawing.Size(100, 20)
+        Me.txtCodePostal.TabIndex = 5
+        Me.txtCodePostal.Text = "Code Postal"
+        '
+        'txtVille
+        '
+        Me.txtVille.Location = New System.Drawing.Point(87, 343)
+        Me.txtVille.Name = "txtVille"
+        Me.txtVille.Size = New System.Drawing.Size(100, 20)
+        Me.txtVille.TabIndex = 6
+        Me.txtVille.Text = "Ville"
+        '
+        'dtpEmbauche
+        '
+        Me.dtpEmbauche.Location = New System.Drawing.Point(87, 406)
+        Me.dtpEmbauche.Name = "dtpEmbauche"
+        Me.dtpEmbauche.Size = New System.Drawing.Size(200, 20)
+        Me.dtpEmbauche.TabIndex = 7
+        '
+        'cmbRole
+        '
+        Me.cmbRole.FormattingEnabled = True
+        Me.cmbRole.Location = New System.Drawing.Point(87, 467)
+        Me.cmbRole.Name = "cmbRole"
+        Me.cmbRole.Size = New System.Drawing.Size(121, 21)
+        Me.cmbRole.TabIndex = 8
+        Me.cmbRole.Text = "Rôle"
+        '
+        'btnValider
+        '
+        Me.btnValider.Location = New System.Drawing.Point(87, 525)
+        Me.btnValider.Name = "btnValider"
+        Me.btnValider.Size = New System.Drawing.Size(75, 23)
+        Me.btnValider.TabIndex = 9
+        Me.btnValider.Text = "Valider"
+        Me.btnValider.UseVisualStyleBackColor = True
+        '
+        'FormCreerUtilisateur
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1343, 608)
+        Me.Controls.Add(Me.btnValider)
+        Me.Controls.Add(Me.cmbRole)
+        Me.Controls.Add(Me.dtpEmbauche)
+        Me.Controls.Add(Me.txtVille)
+        Me.Controls.Add(Me.txtCodePostal)
+        Me.Controls.Add(Me.txtAdresse)
+        Me.Controls.Add(Me.txtMdp)
+        Me.Controls.Add(Me.txtLogin)
+        Me.Controls.Add(Me.txtPrenom)
+        Me.Controls.Add(Me.txtNom)
+        Me.Name = "FormCreerUtilisateur"
         Me.Text = "FormCreerUtilisateur"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents txtNom As TextBox
+    Friend WithEvents txtPrenom As TextBox
+    Friend WithEvents txtLogin As TextBox
+    Friend WithEvents txtMdp As TextBox
+    Friend WithEvents txtAdresse As TextBox
+    Friend WithEvents txtCodePostal As TextBox
+    Friend WithEvents txtVille As TextBox
+    Friend WithEvents dtpEmbauche As DateTimePicker
+    Friend WithEvents cmbRole As ComboBox
+    Friend WithEvents btnValider As Button
 End Class
--- gsb_ap8/FormCreerUtilisateur.vb
+++ gsb_ap8/FormCreerUtilisateur.vb
@@ -1,3 +1,68 @@
-﻿Public Class FormCreerUtilisateur
+﻿Imports MySql.Data.MySqlClient
 
+Public Class FormCreerUtilisateur
+    Private Sub FormCreerUtilisateur_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbRole.Items.Add("Visiteur")
+        cmbRole.Items.Add("Comptable")
+        cmbRole.SelectedIndex = 0
+    End Sub
+
+    Private Sub btnValider_Click(sender As Object, e As EventArgs) Handles btnValider.Click
+        ' Vérification des champs obligatoires
+        If txtLogin.Text = "" OrElse txtNom.Text = "" OrElse txtPrenom.Text = "" OrElse txtLogin.Text = "" OrElse txtMdp.Text = "" OrElse cmbRole.Text = "" Then
+            MessageBox.Show("Veuillez remplir tous les champs obligatoires.", "Erreur",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        Try
+            Dim mdpHash = HashSHA256(txtMdp.Text)
+
+            Dim requete = "INSERT INTO utilisateur (id, nom, prenom, login, mdp, adresse, cp, ville, dateEmbauche) VALUES (@id, @nom, @prenom, @login, @mdp, @adresse, @cp, @ville, @dateEmbauche)"
+            Dim cmd As New MySqlCommand(requete, ConnexionBDD.GetConnexion())
+            cmd.Parameters.AddWithValue("@id", txtLogin.Text)
+            cmd.Parameters.AddWithValue("@nom", txtNom.Text)
+            cmd.Parameters.AddWithValue("@prenom", txtPrenom.Text)
+            cmd.Parameters.AddWithValue("@login", txtLogin.Text)
+            cmd.Parameters.AddWithValue("@mdp", mdpHash)
+            cmd.Parameters.AddWithValue("@adresse", txtAdresse.Text)
+            cmd.Parameters.AddWithValue("@cp", txtCodePostal.Text)
+            cmd.Parameters.AddWithValue("@ville", txtVille.Text)
+            cmd.Parameters.AddWithValue("@dateEmbauche", dtpEmbauche.Value)
+            cmd.ExecuteNonQuery()
+
+            If cmbRole.Text = "Visiteur" Then
+                Dim requeteVisiteur = "INSERT INTO visiteur (id) VALUES (@id)"
+                Dim cmdVisiteur As New MySqlCommand(requeteVisiteur, ConnexionBDD.GetConnexion())
+                cmdVisiteur.Parameters.AddWithValue("@id", txtLogin.Text)
+                cmdVisiteur.ExecuteNonQuery()
+            Else
+                Dim requeteComptable = "INSERT INTO comptable (id, nbFichesRefusees) VALUES (@id, 0)"
+                Dim cmdComptable As New MySqlCommand(requeteComptable, ConnexionBDD.GetConnexion())
+                cmdComptable.Parameters.AddWithValue("@id", txtLogin.Text)
+                cmdComptable.ExecuteNonQuery()
+            End If
+
+            MessageBox.Show("Utilisateur créé avec succès !", "Succès",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            ' Vider les champs
+            txtLogin.Clear()
+            txtNom.Clear()
+            txtPrenom.Clear()
+            txtLogin.Clear()
+            txtMdp.Clear()
+            txtAdresse.Clear()
+            txtCodePostal.Clear()
+            txtVille.Clear()
+
+        Catch ex As MySqlException
+            ' Erreur SQL
+            MessageBox.Show("Erreur BDD : " & ex.Message, "Erreur",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            MessageBox.Show("Erreur : " & ex.Message, "Erreur",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 End Class
